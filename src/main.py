00e01d64dae78c1fcd6dc93c725b2abe2c551350
Rougert Brian Alexandre <rougert.brian@gmail.com>
2024-01-17 13:12:41 -0300

    venv

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+"""
+Pegar os dados da planilha 
+Tipo nome do curso, nome participante, tipo de participação, data do inicio, data do final, carga horária, data da emissão do certificado
+
+Transferir os dados da planilha para a imagem do certificado
+"""
+import openpyxl
+
+workbook_alunos = openpyxl.load_workbook("src\\assets\\planilha_alunos.xlsx")
+
+print(workbook_alunos)
+
